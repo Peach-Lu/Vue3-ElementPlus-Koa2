e2696e2a756e86d6467f04bc11c0ded234f806d5
Peach <2990221345@qq.com>
2022-09-14 01:01:42 +0800

    update

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>login</div>
+  <el-button @click="goHome">回首页</el-button>
+</template>
+
+<script setup lang="ts">
+import router from '../router'
+
+const goHome = () => {
+  router.push({
+    path: '/welcome'
+  })
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Welcome</div>
+  <h1>欢迎来到主界面</h1>
+  <router-link to="/login">去登录</router-link>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped></style>
