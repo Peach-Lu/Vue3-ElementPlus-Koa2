544f5dd23ec2d70c957287dc6aea80278e90b5b7
Peach <2990221345@qq.com>
2022-12-20 23:08:25 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    // host: 'localhost',
+    host: 'localhost',
     port: 8080,
     proxy: {
       // '/api': {
